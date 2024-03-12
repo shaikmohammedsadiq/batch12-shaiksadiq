7a329fb0a87fb465e6dd20421d14bbdc6e16c2d1
shaikmohammedsadiq <162983314+shaikmohammedsadiq@users.noreply.github.com>
2024-03-12 16:01:03 +0530

    Add files via upload

---
+++ file_3.py
@@ -0,0 +1,18 @@
+# a=b c=4,2
+# print(a,b,c)
+# ====> swapping of variables
+a = 7
+b = 8
+
+
+temp=a
+a=b
+b=temp
+
+
+# a=7, b=8
+print(a, b)
+print("hello world")
+import keyword
+print(keyword.kwlist)
+      
